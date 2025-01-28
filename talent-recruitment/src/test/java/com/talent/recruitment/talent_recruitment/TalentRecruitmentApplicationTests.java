afd55f5de229c92b400aae48060a87a3124fb365
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-28 12:22:49 +0530

    Update TalentRecruitmentApplicationTests.java

--- talent-recruitment/src/test/java/com/talent/recruitment/talent_recruitment/TalentRecruitmentApplicationTests.java
+++ talent-recruitment/src/test/java/com/talent/recruitment/talent_recruitment/TalentRecruitmentApplicationTests.java
@@ -3,6 +3,8 @@ package com.talent.recruitment.talent_recruitment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class TalentRecruitmentApplicationTests {
 
